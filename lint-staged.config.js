2e6ce627e97cfe6a62b16e2dc3892baf69e6b9dd
Filip Derenowski <fderen.dev@gmail.com>
2022-07-21 22:10:12 +0200

    Added stylelint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,6 +6,13 @@ module.exports = {
     `yarn prettier --write ${filenames.join(" ")}`,
   ],
 
+  "**/*.(scss)": (filenames) => [`yarn stylelint --fix ${filenames.join(" ")}`],
+
+  "**/*.(ts|tsx|js)": (filenames) => [
+    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
   "**/*.(md|json)": (filenames) =>
     `yarn prettier --write ${filenames.join(" ")}`,
 };
