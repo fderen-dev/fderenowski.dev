4bcd9d428762eeefd2a800db77507108691688bc
Filip Derenowski <fderen.dev@gmail.com>
2022-07-21 21:53:33 +0200

    Added prettier and pre commit config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  eslint: {
+    dirs: ["pages/", "components/", "src/", "utils/"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
