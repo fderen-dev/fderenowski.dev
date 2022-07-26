b1dc6efc7320c65961546351fd705d550f807c67
Filip Derenowski <fderen.dev@gmail.com>
2022-07-26 23:18:14 +0200

    Added sass loader paths config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
     dirs: ["pages/", "components/", "src/", "utils/"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
