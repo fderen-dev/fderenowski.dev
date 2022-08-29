24023f9e4681e95bbb7de6fc562a7a8aa527769e
Filip Derenowski <fderen.dev@gmail.com>
2022-08-29 23:59:50 +0200

    Added svg support

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
