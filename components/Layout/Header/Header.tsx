da9b3f524528a7720544762326e51712b5fe164d
Filip Derenowski <fderen.dev@gmail.com>
2022-09-01 23:45:44 +0200

    Limited header to max page width

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,10 +1,12 @@
 import styles from "./header.module.scss";
 
 export const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.headingContainer}>
-      <h1 className={styles.heading}>Filip Derenowski</h1>
-      <div className={styles.subheading}>Frontend developer</div>
-    </div>
-  </header>
+  <div className={styles.container}>
+    <header className={styles.header}>
+      <div className={styles.headingContainer}>
+        <h1 className={styles.heading}>Filip Derenowski</h1>
+        <div className={styles.subheading}>Frontend developer</div>
+      </div>
+    </header>
+  </div>
 );
