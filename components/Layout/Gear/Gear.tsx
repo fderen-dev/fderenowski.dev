62fe21d6a707bd0438de2f426ef99118f22551a7
Filip Derenowski <fderen.dev@gmail.com>
2022-09-03 15:53:41 +0200

    Added responsive placement for Gear

--- components/Layout/Gear/Gear.tsx
+++ components/Layout/Gear/Gear.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import classNames from "classnames/bind";
+import { useMediaQuery } from "react-responsive";
 
 import { ScrollDirection, useScrollDetection } from "utils/useScrollDetection";
 
@@ -9,22 +10,45 @@ import styles from "./gear.module.scss";
 
 const cx = classNames.bind(styles);
 
+export type Placement = {
+  top: string;
+  left: string;
+};
+
+export type ResponsivePlacement = {
+  mobiles: Placement;
+  desktop: Placement;
+};
+
 interface GearProps {
   maxSize?: string;
   minSize?: string;
-  top?: string;
-  left?: string;
+  placement: Placement;
+  responsivePlacement?: ResponsivePlacement;
   className?: string;
 }
 
 export const Gear: FC<GearProps> = ({
-  top = "10vh",
-  left = "60%",
+  placement,
+  responsivePlacement,
   maxSize = "20%",
   minSize = "150px",
   className,
 }) => {
   const { isScrolling, scrollDirection } = useScrollDetection(100);
+  const isDesktop = useMediaQuery({ query: "(min-width: 992px)" });
+  let { top, left } = placement;
+
+  if (responsivePlacement) {
+    const { desktop, mobiles } = responsivePlacement;
+    if (isDesktop) {
+      top = desktop.top;
+      left = desktop.left;
+    } else {
+      top = mobiles.top;
+      left = mobiles.left;
+    }
+  }
 
   return (
     <GearImage
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
 
-import { Gear } from "./Gear/Gear";
+import { Gear, Placement, ResponsivePlacement } from "./Gear/Gear";
 import { Header } from "./Header/Header";
 
 import styles from "./layout.module.scss";
+
+const DESKTOP_GEAR_PLACEMENT: Placement = { top: "10vh", left: "60%" };
+const RESPONSIVE_GEAR_PLACEMENT: ResponsivePlacement = {
+  mobiles: { top: "25vh", left: "60%" },
+  desktop: DESKTOP_GEAR_PLACEMENT,
+};
 
 export const Layout = ({ children }: { children: ReactNode }) => (
   <div className={styles.root}>
@@ -12,7 +18,10 @@ export const Layout = ({ children }: { children: ReactNode }) => (
       <div className={styles.transition} />
       <div className={styles.contentWrapper}>
         <div className={styles.content}>
-          <Gear />
+          <Gear
+            placement={DESKTOP_GEAR_PLACEMENT}
+            responsivePlacement={RESPONSIVE_GEAR_PLACEMENT}
+          />
           {children}
         </div>
       </div>
