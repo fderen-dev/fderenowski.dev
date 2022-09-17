3dc18cded14b618fcac523504adfd85551fa7c7e
Filip Derenowski <fderen.dev@gmail.com>
2022-09-17 21:16:29 +0200

    Added react modal, Navlist init

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,6 +1,51 @@
 import { useState } from "react";
-import { useMediaQuery } from "react-responsive";
+import Link from "next/link";
+import classNames from "classnames";
+import ReactModal from "react-modal";
+
+import { useMediaQueriesContext } from "context/MediaQueries";
+import { ROUTES } from "utils/constants";
 
 import styles from "./navbar.module.scss";
 
-export const Navbar = () => <nav className={styles.navbar}></nav>;
+interface NavListItemProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const NavListItem = ({ href, children }: NavListItemProps) => (
+  <li>
+    <Link href={href}>{children}</Link>
+  </li>
+);
+
+interface NavListProps {
+  className?: string;
+}
+
+const NavList = ({ className }: NavListProps) => (
+  <ul className={classNames(styles.navList, className)}>
+    <NavListItem href={ROUTES.HOME}>Home</NavListItem>
+  </ul>
+);
+
+export const Navbar = () => {
+  const { isDesktop } = useMediaQueriesContext();
+  const [isMobileNavigationOpen, setMobileNavigationOpen] = useState(false);
+  const toggleMobileNavigation = () => setMobileNavigationOpen((prev) => !prev);
+
+  return (
+    <>
+      <nav className={styles.navbar}>
+        {isDesktop ? (
+          <NavList className={styles.horizontal} />
+        ) : (
+          <button onClick={toggleMobileNavigation}>Hamburger</button>
+        )}
+      </nav>
+      <ReactModal isOpen={isMobileNavigationOpen}>
+        <NavList className={styles.vertical} />
+      </ReactModal>
+    </>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from "next/app";
+import Modal from "react-modal";
+
 import { MediaQueriesProvider } from "context/MediaQueries";
 
 import "styles/global.scss";
+
+Modal.setAppElement("#__next");
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
