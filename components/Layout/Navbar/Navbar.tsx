e601bc89277e0860148e517b99d8f72ef012f46e
Filip Derenowski <fderen.dev@gmail.com>
2022-09-30 18:38:33 +0200

    Added logo as home route

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -3,47 +3,74 @@ import Link from "next/link";
 import classNames from "classnames";
 import ReactModal from "react-modal";
 
+import { ForDesktop, NotForDesktop } from "components/MediaQueriesComponents";
+
 import { useMediaQueriesContext } from "context/MediaQueries";
 import { ROUTES } from "utils/constants";
 import { useIsMounted } from "utils/useIsMounted";
 
+import GearImage from "../../../public/static/gear.svg";
+
 import styles from "./navbar.module.scss";
 
-interface NavListItemProps {
+interface NavigationItemProps {
   href: string;
   children: React.ReactNode;
+  As?: "li" | "div" | "span";
+  containerClassName?: string;
+  linkClassName?: string;
 }
 
-const NavListItem = ({ href, children }: NavListItemProps) => (
-  <li>
-    <Link href={href}>{children}</Link>
-  </li>
+const NavigationItem = ({
+  href,
+  children,
+  As = "li",
+  containerClassName,
+  linkClassName,
+}: NavigationItemProps) => (
+  <As className={classNames(styles.navigationItem, containerClassName)}>
+    <Link passHref href={href}>
+      <a className={classNames(styles.navigationItemLink, linkClassName)}>
+        {children}
+      </a>
+    </Link>
+  </As>
 );
 
-interface NavListProps {
+interface NavigationListProps {
   className?: string;
 }
 
-const NavList = ({ className }: NavListProps) => (
-  <ul className={classNames(styles.navList, className)}>
-    <NavListItem href={ROUTES.HOME}>Home</NavListItem>
-  </ul>
+const NavigationList = ({ className }: NavigationListProps) => (
+  <ul className={classNames(styles.navigationList, className)}></ul>
+);
+
+const HomeRoute = () => (
+  <NavigationItem
+    As="span"
+    href={ROUTES.HOME}
+    containerClassName={styles.homeRoute}
+  >
+    <GearImage alt="Logo" className={styles.logo} />
+  </NavigationItem>
 );
 
 interface NavbarContentProps {
-  isDesktop: boolean;
   toggleMobileNavigation: () => void;
 }
 
-const NavbarContent = ({
-  isDesktop,
-  toggleMobileNavigation,
-}: NavbarContentProps) => {
-  if (isDesktop) {
-    return <NavList className={styles.horizontal} />;
-  }
-
-  return <button onClick={toggleMobileNavigation}>Hamburger</button>;
+const NavbarContent = ({ toggleMobileNavigation }: NavbarContentProps) => {
+  return (
+    <div className={styles.navbarContent}>
+      <HomeRoute />
+      <ForDesktop>
+        <NavigationList className={styles.horizontal} />
+      </ForDesktop>
+      <NotForDesktop>
+        <button onClick={toggleMobileNavigation}>Hamburger</button>
+      </NotForDesktop>
+    </div>
+  );
 };
 
 export const Navbar = () => {
@@ -60,14 +87,11 @@ export const Navbar = () => {
     <>
       <nav className={styles.navbar}>
         {isMounted && (
-          <NavbarContent
-            isDesktop={isDesktop}
-            toggleMobileNavigation={toggleMobileNavigation}
-          />
+          <NavbarContent toggleMobileNavigation={toggleMobileNavigation} />
         )}
       </nav>
       <ReactModal isOpen={isMobileNavigationOpen}>
-        <NavList className={styles.vertical} />
+        <NavigationList className={styles.vertical} />
       </ReactModal>
     </>
   );
