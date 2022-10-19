54a288124858f9a4ed57a7c36aad42cff91a5ceb
fderen-dev <fderen.dev@gmail.com>
2022-10-19 22:28:16 +0200

    Changed gear position

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,4 +1,12 @@
+import { Gear, Placement, ResponsivePlacement } from "../Gear/Gear";
+
 import styles from "./header.module.scss";
+
+const DESKTOP_GEAR_PLACEMENT: Placement = { top: "10vh", left: "60%" };
+const RESPONSIVE_GEAR_PLACEMENT: ResponsivePlacement = {
+  mobiles: { top: "25vh", left: "60%" },
+  desktop: DESKTOP_GEAR_PLACEMENT,
+};
 
 export const Header = () => (
   <div className={styles.container}>
@@ -7,6 +15,10 @@ export const Header = () => (
         <h1 className={styles.heading}>Filip Derenowski</h1>
         <div className={styles.subheading}>Frontend developer</div>
       </div>
+      <Gear
+        placement={DESKTOP_GEAR_PLACEMENT}
+        responsivePlacement={RESPONSIVE_GEAR_PLACEMENT}
+      />
     </header>
   </div>
 );
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,27 +3,16 @@ import { ReactNode } from "react";
 import { WithChildren } from "utils/types";
 
 import { Footer } from "./Footer/Footer";
-import { Gear, Placement, ResponsivePlacement } from "./Gear/Gear";
 import { Header } from "./Header/Header";
 import { Navbar } from "./Navbar/Navbar";
 
 import styles from "./layout.module.scss";
-
-const DESKTOP_GEAR_PLACEMENT: Placement = { top: "10vh", left: "60%" };
-const RESPONSIVE_GEAR_PLACEMENT: ResponsivePlacement = {
-  mobiles: { top: "25vh", left: "60%" },
-  desktop: DESKTOP_GEAR_PLACEMENT,
-};
 
 const Main = ({ children }: WithChildren) => (
   <main className={styles.main}>
     <div className={styles.transition} />
     <div className={styles.contentWrapper}>
       <div className={styles.content}>
-        <Gear
-          placement={DESKTOP_GEAR_PLACEMENT}
-          responsivePlacement={RESPONSIVE_GEAR_PLACEMENT}
-        />
         {children}
       </div>
     </div>
