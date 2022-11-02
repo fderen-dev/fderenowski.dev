105d34e16823ec02a41c640f03cbedd83dc5542c
fderen-dev <fderen.dev@gmail.com>
2022-11-02 19:58:18 +0100

    Adjusted about page styles

--- pages/about.tsx
+++ pages/about.tsx
@@ -21,26 +21,27 @@ const About: NextPage = () => {
             src={portrait}
             alt="Picture of the author"
             sizes="(max-width: 1920px) 80vw, 800px"
+            priority
             className={styles.portrait}
           />
-          <h2 className={styles.subheader}>
-            <span>About me</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
-            risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
-            nibh a vulputate placerat. Aenean id elit vitae quam malesuada
-            volutpat a euismod dolor. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Nam luctus finibus sem, eu rhoncus augue aliquet
-            sit amet. Aliquam pharetra consequat ex non pulvinar. Praesent
-            pellentesque dui sit amet tortor imperdiet, id pretium lorem porta.
-            Phasellus eu molestie metus. Vivamus vulputate turpis eu tristique
-            suscipit. Fusce id commodo nibh. Vestibulum pharetra lacus nec
-            laoreet vestibulum. Praesent malesuada turpis eget nibh posuere, at
-            auctor justo aliquam. Suspendisse vel vestibulum lacus, non bibendum
-            nisl. Nulla ut hendrerit dolor. Praesent tristique vel lectus non
-            dictum.
-          </p>
+          <div className={styles.content}>
+            <h2 className={styles.subheader}>About me</h2>
+            <p className={styles.paragraph}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
+              risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
+              nibh a vulputate placerat. Aenean id elit vitae quam malesuada
+              volutpat a euismod dolor. Interdum et malesuada fames ac ante
+              ipsum primis in faucibus. Nam luctus finibus sem, eu rhoncus augue
+              aliquet sit amet. Aliquam pharetra consequat ex non pulvinar.
+              Praesent pellentesque dui sit amet tortor imperdiet, id pretium
+              lorem porta. Phasellus eu molestie metus. Vivamus vulputate turpis
+              eu tristique suscipit. Fusce id commodo nibh. Vestibulum pharetra
+              lacus nec laoreet vestibulum. Praesent malesuada turpis eget nibh
+              posuere, at auctor justo aliquam. Suspendisse vel vestibulum
+              lacus, non bibendum nisl. Nulla ut hendrerit dolor. Praesent
+              tristique vel lectus non dictum.
+            </p>
+          </div>
         </section>
       </Layout>
     </>
