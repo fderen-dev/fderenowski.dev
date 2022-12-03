7859db6f0ccde604dc14b7bb9fde3e1c6604379f
fderen-dev <fderen.dev@gmail.com>
2022-12-03 22:14:08 +0100

    Added Spinner component

---
+++ components/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+import bind from 'classnames/bind'
+
+import styles from './spinner.module.scss';
+
+type Size = "small" | "medium" | "large";
+
+interface SpinnerProps {
+  size: Size;
+  className?: string;
+}
+
+const cx = classNames.bind(styles);
+
+export const Spinner = ({ size = "medium", className }: SpinnerProps) => (
+  <div className={cx(styles.spinner, size, className)}></div>
+);
