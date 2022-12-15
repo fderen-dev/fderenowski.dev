271ef61ddb141c1d4079c884029730f6fdbd87b5
fderen-dev <fderen.dev@gmail.com>
2022-12-15 20:20:59 +0100

    Initially used forms in contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,29 +1,35 @@
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { default as Textarea} from 'react-textarea-autosize';
 
 import { Button } from "components/Button/Button";
+import Forms, { FormStructure } from "components/Forms";
 import { Layout } from "components/Layout/Layout";
 
 import { TimeoutHandle } from "utils/types";
 
 import styles from './contact.module.scss'
 
+interface ContactFormStructure extends FormStructure {
+  name: string;
+  email: string;
+  message: string;
+}
+
 
 const Contact: NextPage = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const timeoutHandle = useRef<TimeoutHandle | null>(null);
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
+  const handleSubmit = (values: ContactFormStructure) => {
     setIsSubmitting(true);
 
     timeoutHandle.current = setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
       setIsSubmitting(false);
     }, 1000);
-  }
+  };
 
   useEffect(() => {
     return (() => {
@@ -46,21 +52,19 @@ const Contact: NextPage = () => {
             Any questions?
             <div className={styles.subheading}>or feedback?</div>
           </h2>
-          <form onSubmit={handleSubmit} className={styles.form}>
-            <div className={styles.formGroup}>
-              <label>
-                <span className={styles.text}>Name</span>
-                <input placeholder="What's your name?" />
-              </label>
-              <label>
-                <span className={styles.text}>E-mail</span>
-                <input
-                  id="email"
+          <Forms.Form onSubmit={handleSubmit}>
+            <Forms.FormGroup>
+                <Forms.Input
+                  name="name"
+                  label="name"
+                  placeholder="What's your name?" />
+                <Forms.Input
                   type="email"
+                  name="email"
+                  label="E-mail"
                   placeholder="Type your email so I could get back to you"
                 />
-              </label>
-            </div>
+            </Forms.FormGroup>
             <label>
               <span className={styles.text}>Message</span>
               <Textarea
@@ -70,7 +74,7 @@ const Contact: NextPage = () => {
               />
             </label>
             <Button type="submit" loading={isSubmitting}>Send!</Button>
-          </form>
+          </Forms.Form>
         </section>
       </Layout>
     </>
