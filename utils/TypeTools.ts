18b4b3bd31f01fdc9049900da2c6212e437b1c93
fderen-dev <fderen.dev@gmail.com>
2023-01-06 00:28:27 +0100

    Added TypeTools

---
+++ utils/TypeTools.ts
@@ -0,0 +1,17 @@
+export class TypeTools {
+  static isNullOrUndefined(value: unknown): boolean {
+    return value === undefined || value === null;
+  }
+
+  static isString(value: unknown): boolean {
+    return typeof value === "string";
+  }
+
+  static isNotEmtpyString(value: unknown): boolean {
+    return this.isString(value) && (value as string).trim().length > 0;
+  }
+
+  static isNonEmptyArray(value: unknown): boolean {
+    return Array.isArray(value) && value.length > 0;
+  }
+}
