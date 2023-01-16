887610226d0828b74ddff398df44d476045cdc09
fderen-dev <fderen.dev@gmail.com>
2023-01-16 21:09:24 +0100

    Added prismic config

--- context/RootProvider.tsx
+++ context/RootProvider.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
+import { PrismicPreview } from "@prismicio/next";
+import { PrismicProvider } from "@prismicio/react";
+
 import { WithChildren } from "utils/types";
 
 import { MediaQueries } from "./MediaQueries";
 import { ScrollDetectionProvider } from "./ScrollDetection";
 
+import { repositoryName } from "../prismicio";
+
 const RootProvider = ({ children }: WithChildren) => (
-  <MediaQueries.Provider>
-    <ScrollDetectionProvider>{children}</ScrollDetectionProvider>
-  </MediaQueries.Provider>
+  <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+    <PrismicPreview repositoryName={repositoryName}>
+      <MediaQueries.Provider>
+        <ScrollDetectionProvider>{children}</ScrollDetectionProvider>
+      </MediaQueries.Provider>
+    </PrismicPreview>
+  </PrismicProvider>
 );
 
 export default RootProvider;
