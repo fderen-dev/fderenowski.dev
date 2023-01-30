cdc7b64d72553e9cd82ffb6dbcafea4f375816e0
fderen-dev <fderen.dev@gmail.com>
2023-01-30 21:35:48 +0100

    CMS homepage init

---
+++ .slicemachine/assets/slices/Section/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Section';
+
+export default {
+  title: 'slices/Section'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading2","text":"Slipped","spans":[]}],"content":[{"type":"paragraph","text":"Do quis velit ad et magna nulla laborum nostrud duis tempor non est sunt. Pariatur incididunt duis in sunt est officia ullamco labore consectetur commodo ut anim. Cupidatat et proident mollit minim consequat ea ex aliqua.","spans":[]}]},"id":"_Default","slice_type":"section"}} />
+_Default.storyName = ''
