f33e8fbb0da158036888002faa0288d31430ff1e
fderen-dev <fderen.dev@gmail.com>
2023-02-04 17:26:54 +0100

    Connected framgent slice with section component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import type { Content } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { Layout } from "components/common/Layout/Layout";
-import { Section } from "components/common/Section/Section";
 
 import { createClient } from "../prismicio";
 import { components as slices } from "../slices";
@@ -50,79 +49,6 @@ const Home: NextPage<Content.HomepageDocument> = (page) => {
       </Head>
       <Layout contentClassName={styles.layoutContent}>
         <SliceZone slices={slicesData} components={slices} />
-        <Section left header="Lorem" className={styles.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
-            risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
-            nibh a vulputate placerat. Aenean id elit vitae quam malesuada
-            volutpat a euismod dolor. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Nam luctus finibus sem, eu rhoncus augue aliquet
-            sit amet. Aliquam pharetra consequat ex non pulvinar. Praesent
-            pellentesque dui sit amet tortor imperdiet, id pretium lorem porta.
-            Phasellus eu molestie metus. Vivamus vulputate turpis eu tristique
-            suscipit. Fusce id commodo nibh. Vestibulum pharetra lacus nec
-            laoreet vestibulum. Praesent malesuada turpis eget nibh posuere, at
-            auctor justo aliquam. Suspendisse vel vestibulum lacus, non bibendum
-            nisl. Nulla ut hendrerit dolor. Praesent tristique vel lectus non
-            dictum.
-          </p>
-        </Section>
-        <Section
-          right
-          withIntersection
-          header="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          className={styles.section}
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
-            risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
-            nibh a vulputate placerat. Aenean id elit vitae quam malesuada
-            volutpat a euismod dolor. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Nam luctus finibus sem, eu rhoncus augue aliquet
-            sit amet. Aliquam pharetra consequat ex non pulvinar. Praesent
-            pellentesque dui sit amet tortor imperdiet, id pretium lorem porta.
-            Phasellus eu molestie metus. Vivamus vulputate turpis eu tristique
-            suscipit. Fusce id commodo nibh. Vestibulum pharetra lacus nec
-            laoreet vestibulum. Praesent malesuada turpis eget nibh posuere, at
-            auctor justo aliquam. Suspendisse vel vestibulum lacus, non bibendum
-            nisl. Nulla ut hendrerit dolor. Praesent tristique vel lectus non
-            dictum.
-          </p>
-        </Section>
-        <Section left withIntersection className={styles.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
-            risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
-            nibh a vulputate placerat. Aenean id elit vitae quam malesuada
-            volutpat a euismod dolor. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Nam luctus finibus sem, eu rhoncus augue aliquet
-            sit amet. Aliquam pharetra consequat ex non pulvinar. Praesent
-            pellentesque dui sit amet tortor imperdiet, id pretium lorem porta.
-            Phasellus eu molestie metus. Vivamus vulputate turpis eu tristique
-            suscipit. Fusce id commodo nibh. Vestibulum pharetra lacus nec
-            laoreet vestibulum. Praesent malesuada turpis eget nibh posuere, at
-            auctor justo aliquam. Suspendisse vel vestibulum lacus, non bibendum
-            nisl. Nulla ut hendrerit dolor. Praesent tristique vel lectus non
-            dictum.
-          </p>
-        </Section>
-        <Section right withIntersection className={styles.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
-            risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
-            nibh a vulputate placerat. Aenean id elit vitae quam malesuada
-            volutpat a euismod dolor. Interdum et malesuada fames ac ante ipsum
-            primis in faucibus. Nam luctus finibus sem, eu rhoncus augue aliquet
-            sit amet. Aliquam pharetra consequat ex non pulvinar. Praesent
-            pellentesque dui sit amet tortor imperdiet, id pretium lorem porta.
-            Phasellus eu molestie metus. Vivamus vulputate turpis eu tristique
-            suscipit. Fusce id commodo nibh. Vestibulum pharetra lacus nec
-            laoreet vestibulum. Praesent malesuada turpis eget nibh posuere, at
-            auctor justo aliquam. Suspendisse vel vestibulum lacus, non bibendum
-            nisl. Nulla ut hendrerit dolor. Praesent tristique vel lectus non
-            dictum.
-          </p>
-        </Section>
       </Layout>
     </>
   );
--- slices/Fragment/index.tsx
+++ slices/Fragment/index.tsx
@@ -1,36 +1,30 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText } from "@prismicio/react";
 
 import { Section } from "components/common/Section/Section";
 
 import styles from "./fragment.module.scss";
 
-const Fragment = ({
-  slice,
-  slices,
-}: SliceComponentProps<Content.FragmentSlice>) => {
-  console.log(slices);
-
+const Fragment = ({ slice }: SliceComponentProps<Content.FragmentSlice>) => {
   if (slice.variation === "homepageFragment") {
     // @ts-ignore
-    return slices.map((sliceToRender: Content.FragmentSlice) => (
-      <Section left header={sliceToRender.primary.header.} className={styles.section} key={sliceToRender.id}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros
-          risus, efficitur non neque in, tempus mattis ipsum. Duis efficitur
-          nibh a vulputate placerat. Aenean id elit vitae quam malesuada
-          volutpat a euismod dolor. Interdum et malesuada fames ac ante ipsum
-          primis in faucibus. Nam luctus finibus sem, eu rhoncus augue aliquet
-          sit amet. Aliquam pharetra consequat ex non pulvinar. Praesent
-          pellentesque dui sit amet tortor imperdiet, id pretium lorem porta.
-          Phasellus eu molestie metus. Vivamus vulputate turpis eu tristique
-          suscipit. Fusce id commodo nibh. Vestibulum pharetra lacus nec laoreet
-          vestibulum. Praesent malesuada turpis eget nibh posuere, at auctor
-          justo aliquam. Suspendisse vel vestibulum lacus, non bibendum nisl.
-          Nulla ut hendrerit dolor. Praesent tristique vel lectus non dictum.
-        </p>
+    return (
+      <Section
+        withIntersection
+        left={slice?.primary?.placement === "Left"}
+        right={slice?.primary?.placement === "Right"}
+        header={slice?.primary?.header?.[0]?.text}
+        className={styles.section}
+        key={slice.id}
+      >
+        {slice.items.map((item) => (
+          // eslint-disable-next-line react/jsx-key
+          <PrismicRichText field={item.content} />
+        ))}
       </Section>
-    ));
+    );
   }
 };
+
 export default Fragment;
