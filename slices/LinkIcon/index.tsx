578c37b12c7fbc3458673d0d5b7bdfade1c79060
fderen-dev <fderen.dev@gmail.com>
2023-02-19 18:34:51 +0100

    Updated linkIcon slice with styles and attributes

--- slices/LinkIcon/index.tsx
+++ slices/LinkIcon/index.tsx
@@ -3,9 +3,20 @@ import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { PrismicLink, SliceComponentProps } from "@prismicio/react";
 
+import styles from "./linkIcon.module.scss";
+
 const LinkIcon = ({ slice }: SliceComponentProps<Content.LinkIconSlice>) => (
-  <PrismicLink field={slice.primary.link}>
-    <PrismicNextImage field={slice.primary.icon} draggable={false} />
+  <PrismicLink
+    rel="noreferrer noopener"
+    target="_blank"
+    field={slice.primary.link}
+    className={styles.link}
+  >
+    <PrismicNextImage
+      field={slice.primary.icon}
+      draggable={false}
+      className={styles.icon}
+    />
   </PrismicLink>
 );
 
