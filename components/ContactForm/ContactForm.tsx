1576ec001b9fdb9422e0df3a714a90b97977d42a
fderen-dev <fderen.dev@gmail.com>
2023-02-28 20:15:11 +0100

    Used Input slices in ContactForm

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,30 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Content } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
 
 import Forms, { FormStructure } from "components/common/Forms";
 import { useFormStateContext } from "components/common/Forms/FormProvider";
-import {
-  isEmailBuilder,
-  isEmptyBuilder,
-  maxLengthBuilder,
-  minLengthBuilder,
-} from "components/common/Forms/validators/simple";
-import { composeValidators } from "components/common/Forms/validators/utils";
 
 import { SubmitButton, SubmitButtonApi } from "./SubmitButton/SubmitButton";
 
-const nameValidator = composeValidators([
-  isEmptyBuilder("Name cannot be empty."),
-  minLengthBuilder("Name has to have at least 3 characters.", 3),
-]);
-const emailValidator = composeValidators([
-  isEmptyBuilder("E-mail is requried."),
-  isEmailBuilder("It seems not to be an e-mail address."),
-]);
-const messageValidator = composeValidators([
-  isEmptyBuilder("Don't leave empty message :("),
-  maxLengthBuilder("Message can have max 500 characters", 500),
-]);
+import { components as slices } from "../../slices";
 
 export interface ContactFormStructure extends FormStructure {
   name: string;
@@ -37,7 +20,7 @@ interface ContactFormProps {
 }
 
 export const ContactForm = ({ prismicDocumentData }: ContactFormProps) => {
-  const { submitlabel: submitLabel, slices } = prismicDocumentData;
+  const { submitlabel: submitLabel, slices: slicesData } = prismicDocumentData;
 
   const abortControllerRef = useRef<AbortController | null>(null);
   const submitButtonApiRef = useRef<SubmitButtonApi | null>(null);
@@ -94,33 +77,7 @@ export const ContactForm = ({ prismicDocumentData }: ContactFormProps) => {
 
   return (
     <Forms.Form validateOnSubmit onSubmit={handleSubmit}>
-      <Forms.FormGroup>
-        <Forms.Input
-          validateOnChange
-          validation={nameValidator}
-          name="name"
-          label="Name"
-          placeholder="What's your name?"
-        />
-        <Forms.Input
-          validateOnChange
-          validation={emailValidator}
-          type="email"
-          name="email"
-          label="E-mail"
-          placeholder="Type your email so I could get back to you"
-        />
-      </Forms.FormGroup>
-      <Forms.TextArea
-        validateOnChange
-        validation={messageValidator}
-        minRows={5}
-        maxRows={8}
-        maxLength={500}
-        name="message"
-        label="Message"
-        placeholder="What's on your mind?"
-      />
+      <SliceZone slices={slicesData} components={slices} />
       <SubmitButton
         text={submitLabel ?? "Submit"}
         loading={isSubmitting}
