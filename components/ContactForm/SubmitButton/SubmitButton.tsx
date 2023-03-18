1a54f1de6c746f6b8e0dc20a8c7dc4be9535f149
fderen-dev <fderen.dev@gmail.com>
2023-03-18 15:15:23 +0100

    Restructored public folder

--- components/ContactForm/SubmitButton/SubmitButton.tsx
+++ components/ContactForm/SubmitButton/SubmitButton.tsx
@@ -10,8 +10,8 @@ import { Button } from "components/common/Button/Button";
 
 import { TimeoutHandle } from "utils/types";
 
-import ErrorIcon from "public/static/cancel.svg";
-import OkIcon from "public/static/ok.svg";
+import ErrorIcon from "public/cancel.svg";
+import OkIcon from "public/ok.svg";
 
 import styles from "./submitButton.module.scss";
 
