7a9406aa799b51efa119b97f6100d27edae67290
fderen-dev <fderen.dev@gmail.com>
2023-03-18 15:14:36 +0100

    Added favicon

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -26,6 +26,13 @@ export const Head = ({ meta, children }: HeadProps) => (
       <meta name="keywords" content={meta.meta_keywords} />
     )}
     {meta.meta_robots && <meta name="robots" content={meta.meta_robots} />}
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
     {children}
   </HeadNext>
 );
