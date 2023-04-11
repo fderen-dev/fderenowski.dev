07713c8ce6c36f10b8243115f1d78935c2333357
fderen-dev <fderen.dev@gmail.com>
2023-04-11 23:44:22 +0200

    Cookie bar init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from "next/app";
 import Modal from "react-modal";
 
+import { CookieBar } from "components/CookieBar/CookieBar";
+import { Measurement } from "components/Measurement/Measurement";
+
 import { MediaQueries } from "context/MediaQueries";
 import RootProvider from "context/RootProvider";
 import { isDevMode } from "utils/utils";
@@ -12,6 +15,8 @@ Modal.setAppElement("#__next");
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RootProvider>
+      <Measurement />
+      <CookieBar />
       <Component {...pageProps} />
       {isDevMode && <MediaQueries.CurrentBreakpoint />}
     </RootProvider>
