344db6333f781e7dd53c0cc0e9e1f9275dd7855a
fderen-dev <fderen.dev@gmail.com>
2023-04-15 17:34:17 +0200

    Added disabled scroll when cookie bar visible

--- components/CookieBar/CookieBar.tsx
+++ components/CookieBar/CookieBar.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useRef } from "react";
 import { Content } from "@prismicio/client";
 import CookieConsent from "react-cookie-consent";
 
 import { Button } from "components/common/Button/Button";
 
-import { isDevMode } from "utils/utils";
+import { useIsMounted } from "utils/useIsMounted";
+import { isDevMode, setBodyScroll } from "utils/utils";
 
 import styles from "./cookieBar.module.scss";
 
@@ -18,6 +20,23 @@ export const CookieBar = ({ prismicDocumentData }: CookieBarProps) => {
     expirationtimeindays = 365,
     content = "I am collecting trafic data with Google Analytics. Your consent will be appreciated.",
   } = prismicDocumentData;
+  const cookieConsentRef = useRef<CookieConsent | null>(null);
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    if (isMounted) {
+      const isCookieBarVisible =
+        cookieConsentRef.current && cookieConsentRef.current.state.visible;
+
+      if (isCookieBarVisible) {
+        setBodyScroll(false);
+      }
+    }
+  }, [isMounted]);
+
+  const enableBodyScroll = () => {
+    setBodyScroll(true);
+  };
 
   return (
     <CookieConsent
@@ -31,6 +50,9 @@ export const CookieBar = ({ prismicDocumentData }: CookieBarProps) => {
       cookieName="cookieConsent"
       debug={isDevMode}
       expires={expirationtimeindays!}
+      onAccept={enableBodyScroll}
+      onDecline={enableBodyScroll}
+      onOverlayClick={enableBodyScroll}
       ButtonComponent={Button}
       customButtonProps={{
         className: styles.button,
@@ -43,6 +65,7 @@ export const CookieBar = ({ prismicDocumentData }: CookieBarProps) => {
       contentClasses={styles.content}
       overlayClasses={styles.overlay}
       buttonWrapperClasses={styles.buttonContainer}
+      ref={cookieConsentRef}
     >
       {content}
     </CookieConsent>
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,1 +1,13 @@
 export const isDevMode = process && process.env.NODE_ENV === "development";
+
+export const setBodyScroll = (isScrollable: boolean) => {
+  const body = document.getElementsByTagName("body").item(0);
+
+  if (body) {
+    isScrollable
+      ? body.classList.remove("overflowHidden")
+      : body.classList.add("overflowHidden");
+  } else {
+    console.log("no body");
+  }
+};
