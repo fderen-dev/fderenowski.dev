883d631de00343c3426af490a2a71754b7892dd7
fderen-dev <fderen.dev@gmail.com>
2023-05-04 22:41:34 +0200

    Updated prismic.io config

--- .slicemachine/assets/slices/LinkIcon/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/LinkIcon';
-
-export default {
-  title: 'slices/LinkIcon'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"href":{"link_type":"Web","url":"https://slicemachine.dev"},"icon":{"dimensions":{"width":32,"height":32},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1606248897732-2c5ffe759c04?rect=0%2C0%2C4000%2C4000&w=32&h=32"}},"id":"_Default","slice_type":"link_icon"}} />
-_Default.storyName = ''
