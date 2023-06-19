9897dc2c5399be74a0f364951bb0f7fcbfd2ead7
fderen-dev <fderen.dev@gmail.com>
2023-06-19 20:11:09 +0200

    Added useFetchPosts hook

--- hooks/blog/useFetchPosts.ts
+++ hooks/blog/useFetchPosts.ts
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 import { BlogpostDocumentWithTags } from "models/blog/BlogpostDocumentWithTags";
 import { FetchReturnType } from "models/misc";
+import { useFetch } from "hooks/useFetch";
 
 const getPostsEndpointUrl = (tags: Array<string>) => {
   const url = new URL(`${origin}/api/posts`);
@@ -15,48 +16,12 @@ const getPostsEndpointUrl = (tags: Array<string>) => {
 export const useFetchPosts = (
   tags: Array<string>
 ): FetchReturnType<Array<BlogpostDocumentWithTags>> => {
-  const [posts, setPosts] = useState<Array<BlogpostDocumentWithTags>>([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    const fetchPosts = async () => {
-      const postsEndpoint = getPostsEndpointUrl(tags);
-
-      setIsFetching(true);
-      try {
-        const response = await fetch(postsEndpoint, {
-          method: "GET",
-          signal: abortController.signal,
-        });
-
-        if (!response.ok) {
-          setError(response.statusText);
-
-          return;
-        }
-
-        const posts: Array<BlogpostDocumentWithTags> = await response.json();
-        setPosts(posts ?? []);
-      } catch (networkError) {
-        setError("Network error");
-      } finally {
-        setIsFetching(false);
-      }
-    };
-
-    if (!abortController.signal.aborted) {
-      fetchPosts();
-    }
-
-    return () => abortController.abort();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [JSON.stringify(tags)]);
+  const url = useMemo(() => getPostsEndpointUrl(tags), [JSON.stringify(tags)]);
+  const { data, isFetching, error } =
+    useFetch<Array<BlogpostDocumentWithTags>>(url);
 
   return {
-    data: posts,
+    data,
     isFetching,
     error,
   };
