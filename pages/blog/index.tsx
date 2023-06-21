710131814ed6065ce15b4ba6876b0404c0b62d9a
fderen-dev <fderen.dev@gmail.com>
2023-06-21 23:17:28 +0200

    BlogPostsList Tags WIP

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { GetStaticProps, NextPage } from "next";
 import { Content } from "@prismicio/client";
 
 import { BlogPostsListContainer } from "components/blog/BlogPostsListContainer/BlogPostsListContainer";
+import { Button } from "components/common/Button/Button";
 import { Head } from "components/common/Head/Head";
 import { Footer, Header, Layout, Navbar } from "components/common/Layout";
 import { CookieBar } from "components/CookieBar/CookieBar";
@@ -64,14 +66,84 @@ const Blog: NextPage<{
         CookieBar={<CookieBar prismicDocumentData={cookieBar.data} />}
         mainClassName={styles.main}
       >
-        <div className={styles.controlsContainer}></div>
         <ClientSideContainer>
-          <BlogPostsListContainer
-            listClassName={styles.postsList}
-            cardClassName={styles.postCard}
-          />
+          <Container />
         </ClientSideContainer>
       </Layout>
+    </>
+  );
+};
+
+function markSelectedTags(
+  availableTags: Array<string>,
+  selectedTags: Array<string>
+): Array<{ label: string; isSelected: boolean }> {
+  return availableTags.map((tag) => ({
+    label: tag,
+    isSelected: selectedTags.includes(tag),
+  }));
+}
+
+interface BlogPostsListTag {
+  label: string;
+  isSelected: boolean;
+}
+
+const TagsList = ({ tags }: { tags: Array<BlogPostsListTag> }) => {
+  return (
+    <ul style={{ display: "flex" }}>
+      {tags.map((tag, index) => (
+        <Tag
+          key={index}
+          tag={tag}
+          onClick={() => {
+            console.log(tag);
+          }}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const Tag = ({
+  tag,
+  onClick,
+  className,
+}: {
+  tag: BlogPostsListTag;
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  className?: string;
+}) => {
+  const { label, isSelected } = tag;
+
+  return (
+    <li className={className}>
+      <Button variant={isSelected ? "primary" : "secondary"} onClick={onClick}>
+        {label}
+      </Button>
+    </li>
+  );
+};
+
+const Container = () => {
+  const selectedTags: Array<string> = new URLSearchParams(
+    window.location.search
+  ).getAll("tag");
+  const availableTags = ["angular", "react.js", "sass"];
+  const [tags] = useState<Array<BlogPostsListTag>>(
+    markSelectedTags(availableTags, selectedTags)
+  );
+
+  return (
+    <>
+      <section className={styles.controlsContainer}>
+        <TagsList tags={tags} />
+      </section>
+      <BlogPostsListContainer
+        selectedTags={selectedTags}
+        listClassName={styles.postsList}
+        cardClassName={styles.postCard}
+      />
     </>
   );
 };
