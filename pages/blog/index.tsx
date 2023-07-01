82de08e82399c3c4e54c104a55e599ab290ffdeb
fderen-dev <fderen.dev@gmail.com>
2023-07-01 14:11:37 +0200

    Created BlogContainer

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,9 @@
-import { useCallback, useMemo, useState } from "react";
 import { GetStaticProps, NextPage } from "next";
 import { Content } from "@prismicio/client";
-import classNames from "classnames";
 
+import { BlogContainer } from "components/blog/BlogContainer/BlogContainer";
 import { BlogPostsListContainer } from "components/blog/BlogPostsListContainer/BlogPostsListContainer";
-import { Button } from "components/common/Button/Button";
+import { TagsList } from "components/blog/TagsList/TagsList";
 import { Head } from "components/common/Head/Head";
 import { Footer, Header, Layout, Navbar } from "components/common/Layout";
 import { CookieBar } from "components/CookieBar/CookieBar";
@@ -81,121 +80,31 @@ const Blog: NextPage<{
         mainClassName={styles.main}
       >
         <ClientSideContainer>
-          {blogPostsTags && <Container blogPostsTags={blogPostsTags} />}
+          {blogPostsTags && (
+            <BlogContainer
+              blogPostsTags={blogPostsTags}
+              renderTagsList={(tags, toggleTagSelected) => (
+                <TagsList
+                  tags={tags}
+                  onTagClick={toggleTagSelected}
+                  className={styles.tagsListWrapper}
+                />
+              )}
+              renderBlogPostsListContainer={(
+                selectedTagsPaths,
+                toggleTagSelected
+              ) => (
+                <BlogPostsListContainer
+                  selectedTagsPaths={selectedTagsPaths}
+                  onTagPillClick={toggleTagSelected}
+                  listClassName={styles.postsList}
+                  cardClassName={styles.postCard}
+                />
+              )}
+            />
+          )}
         </ClientSideContainer>
       </Layout>
-    </>
-  );
-};
-
-function markSelectedTags(
-  availableTags: Array<Tag>,
-  selectedTags: Array<string>
-): Array<BlogPostsListTag> {
-  return availableTags.map((tag) => ({
-    ...tag,
-    isSelected: Boolean(
-      selectedTags.find((selectedTag) => selectedTag === tag.path)
-    ),
-  }));
-}
-
-export interface BlogPostsListTag extends Tag {
-  id: string;
-  url: string;
-  name: string;
-  isSelected: boolean;
-}
-
-interface TagListProps {
-  tags: Array<BlogPostsListTag>;
-  onTagClick: (tag: BlogPostsListTag) => void;
-  className?: string;
-}
-
-const TagsList = ({ tags, onTagClick, className }: TagListProps) => {
-  return (
-    <ul
-      style={{ display: "flex" }}
-      className={classNames(styles.tagsList, className)}
-    >
-      {tags.map((tag) => (
-        <Tag key={tag.id} tag={tag} onClick={() => onTagClick(tag)} />
-      ))}
-    </ul>
-  );
-};
-
-const Tag = ({
-  tag,
-  onClick,
-  className,
-}: {
-  tag: BlogPostsListTag;
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  className?: string;
-}) => {
-  const { name, isSelected } = tag;
-
-  return (
-    <li className={className}>
-      <Button variant={isSelected ? "primary" : "secondary"} onClick={onClick}>
-        {name}
-      </Button>
-    </li>
-  );
-};
-
-interface ContainerProps {
-  blogPostsTags: Array<Tag>;
-}
-
-const Container = ({ blogPostsTags }: ContainerProps) => {
-  const selectedTags: Array<string> = new URLSearchParams(
-    window.location.search
-  ).getAll("tag");
-  const [tags, setTags] = useState<Array<BlogPostsListTag>>(
-    markSelectedTags(blogPostsTags, selectedTags)
-  );
-
-  const toggleTagSelected = useCallback((tag: BlogPostsListTag | Tag) => {
-    setTags((prev) => {
-      const selectedTagIndex = prev.findIndex((_tag) => _tag.id === tag.id);
-
-      if (selectedTagIndex === -1) {
-        return prev;
-      }
-
-      const selectedTag = prev[selectedTagIndex];
-      selectedTag.isSelected = !selectedTag.isSelected;
-
-      return structuredClone(prev);
-    });
-  }, []);
-
-  const selectedTagsPaths = useMemo(() => {
-    return tags.reduce((tagsPaths, tag) => {
-      if (tag.isSelected) {
-        return [...tagsPaths, tag.path];
-      }
-
-      return tagsPaths;
-    }, [] as Array<string>);
-  }, [JSON.stringify(tags)]);
-
-  return (
-    <>
-      <TagsList
-        tags={tags}
-        onTagClick={toggleTagSelected}
-        className={styles.tagsListWrapper}
-      />
-      <BlogPostsListContainer
-        selectedTagsPaths={selectedTagsPaths}
-        onTagPillClick={toggleTagSelected}
-        listClassName={styles.postsList}
-        cardClassName={styles.postCard}
-      />
     </>
   );
 };
