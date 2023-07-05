f9bbc84d7fd6bf64684e5582a4b2e7ca20cd9abb
fderen-dev <fderen.dev@gmail.com>
2023-07-05 23:07:37 +0200

    Added color prop to Spinner component

--- components/common/Spinner/Spinner.tsx
+++ components/common/Spinner/Spinner.tsx
@@ -2,15 +2,21 @@ import classNames from "classnames/bind";
 
 import styles from "./spinner.module.scss";
 
-type Size = "small" | "medium" | "large";
+type Size = "small" | "medium" | "big" | "large";
+type Color = "black" | "yellow";
 
 interface SpinnerProps {
-  size: Size;
+  size?: Size;
+  color?: Color;
   className?: string;
 }
 
 const cx = classNames.bind(styles);
 
-export const Spinner = ({ size = "medium", className }: SpinnerProps) => (
-  <i className={cx(styles.spinner, size, className)}></i>
+export const Spinner = ({
+  size = "medium",
+  color = "black",
+  className,
+}: SpinnerProps) => (
+  <i className={cx(styles.spinner, size, color, className)}></i>
 );
