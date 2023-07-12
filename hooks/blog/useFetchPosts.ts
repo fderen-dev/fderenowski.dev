d86fa28c25856dd59b0901d008f1738a603a4a22
fderen-dev <fderen.dev@gmail.com>
2023-07-12 20:19:22 +0200

    Updated posts endpoint and useFetch hook with page and limit params

--- hooks/blog/useFetchPosts.ts
+++ hooks/blog/useFetchPosts.ts
@@ -3,22 +3,37 @@ import { PaginatedPosts } from "pages/api/posts";
 
 import { FetchReturnType } from "models/misc";
 import { useFetch } from "hooks/useFetch";
+import { TypeTools } from "utils/TypeTools";
 
-const getPostsEndpointUrl = (tagsPaths: Array<string>) => {
+const getPostsEndpointUrl = (
+  tagsPaths: Array<string>,
+  page?: number,
+  limit?: number
+) => {
   const url = new URL(`${origin}/api/posts`);
   tagsPaths.forEach((path) => {
     url.searchParams.append("tag", path);
   });
 
+  if (!TypeTools.isNullOrUndefined(page)) {
+    url.searchParams.append("page", page!.toString());
+  }
+
+  if (!TypeTools.isNullOrUndefined(limit)) {
+    url.searchParams.append("limit", limit!.toString());
+  }
+
   return url;
 };
 
 export const useFetchPosts = (
-  tagsPaths: Array<string>
+  tagsPaths: Array<string>,
+  page?: number,
+  limit?: number
 ): FetchReturnType<PaginatedPosts> => {
   const url = useMemo(
-    () => getPostsEndpointUrl(tagsPaths),
-    [JSON.stringify(tagsPaths)]
+    () => getPostsEndpointUrl(tagsPaths, page, limit),
+    [JSON.stringify(tagsPaths), page, limit]
   );
   const { data, isFetching, error } = useFetch<PaginatedPosts>(url);
 
