d22c98b50ccc8f8df6e05c73e2592e81928b0047
fderen-dev <fderen.dev@gmail.com>
2023-07-16 18:57:00 +0200

    Added separated hook to handle paginated data fetch

---
+++ hooks/useFetchPaginatedData.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+import { FetchReturnType } from "models/misc";
+import { PaginatedResponse } from "pages/api/posts";
+
+export const useFetchPaginatedData = <Data>(
+  url: URL
+): FetchReturnType<PaginatedResponse<Data>> => {
+  const [result, setResult] = useState<PaginatedResponse<Data> | null>(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
+
+    const abortController = new AbortController();
+    const fetchPosts = async () => {
+      setIsFetching(true);
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          signal: abortController.signal,
+        });
+
+        if (!response.ok) {
+          setError(response.statusText);
+
+          return;
+        }
+
+        const result: PaginatedResponse<Data> = await response.json();
+
+        setResult((prev) => {
+          if (!result) {
+            return prev;
+          }
+
+          if (!prev) {
+            return result;
+          }
+
+          return {
+            ...result,
+            data: [...prev!.data, ...result.data],
+          };
+        });
+        setError(null);
+      } catch (networkError) {
+        setError("Network error");
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    if (!abortController.signal.aborted) {
+      fetchPosts();
+    }
+
+    return () => abortController.abort();
+  }, [url]);
+
+  return {
+    data: result,
+    isFetching,
+    error,
+  };
+};
