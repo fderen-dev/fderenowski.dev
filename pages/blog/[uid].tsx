7a530614db87c19bbbf26c4564ff85517a082714
fderen-dev <fderen.dev@gmail.com>
2023-07-18 21:00:33 +0200

    Blogpost layout init

--- pages/blog/[uid].tsx
+++ pages/blog/[uid].tsx
@@ -4,7 +4,12 @@ import * as prismicH from "@prismicio/helpers";
 import { SliceZone } from "@prismicio/react";
 import { components as slices } from "slices";
 
+import { Footer, Layout, Navbar } from "components/common/Layout";
+import { CookieBar } from "components/CookieBar/CookieBar";
+
 import { createClient } from "../../prismicio";
+
+import styles from "./blogPost.module.scss";
 
 // Fetch content from prismic
 export const getStaticProps: GetStaticProps = async ({
@@ -14,9 +19,21 @@ export const getStaticProps: GetStaticProps = async ({
   const client = createClient({ previewData });
 
   const page = await client.getByUID("blogpost", params!.uid as string);
+  const navigation = await client.getByUID<Content.NavigationDocument>(
+    "navigation",
+    "top-navigation"
+  );
+  const footer = await client.getByUID<Content.FooterDocument>(
+    "footer",
+    "footer"
+  );
+  const cookieBar = await client.getByUID<Content.CookiebarDocument>(
+    "cookiebar",
+    "cookie-bar"
+  );
 
   return {
-    props: { page },
+    props: { page, navigation, footer, cookieBar },
   };
 };
 
@@ -33,13 +50,22 @@ export const getStaticPaths = async () => {
 
 const BlogPost: NextPage<{
   page: Content.BlogpostDocument;
-}> = ({ page }) => {
+  navigation: Content.NavigationDocument;
+  footer: Content.FooterDocument;
+  cookieBar: Content.CookiebarDocument;
+}> = ({ page, navigation, footer, cookieBar }) => {
   const { name, header, datecreated, slices: slicesData } = page?.data ?? {};
 
   const date = datecreated ? new Date(datecreated).toDateString() : null;
 
   return (
-    <main>
+    <Layout
+      Navbar={<Navbar prismicDocumentData={navigation.data} />}
+      Footer={<Footer prismicDocumentData={footer.data} />}
+      CookieBar={<CookieBar prismicDocumentData={cookieBar.data} />}
+      contentWrapperClassName={styles.layoutContentWrapper}
+      contentContainerClassName={styles.layoutContentContainer}
+    >
       <header style={{ display: "flex", alignItems: "start" }}>
         <h1 style={{ flexGrow: 1 }}>{name}</h1>
         {date && <time>{date}</time>}
@@ -48,7 +74,7 @@ const BlogPost: NextPage<{
       <section>
         <SliceZone slices={slicesData} components={slices} />
       </section>
-    </main>
+    </Layout>
   );
 };
 
