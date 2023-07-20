7776891d75242db65370d26fc4af96beb91adb24
fderen-dev <fderen.dev@gmail.com>
2023-07-20 20:55:01 +0200

    Blog post page content prototype

--- slices/BlogPostContent/index.tsx
+++ slices/BlogPostContent/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps, SliceComponentType } from "@prismicio/react";
+import {
+  PrismicRichText,
+  SliceComponentProps,
+  SliceComponentType,
+} from "@prismicio/react";
 
 const BlogPostContent: SliceComponentType = (
   props: SliceComponentProps<Content.BlogPostContentSlice>
-) => <section>Blog post content</section>;
+) => {
+  const { slice } = props;
+
+  return <PrismicRichText field={slice.items[0].content} />;
+};
 
 export default BlogPostContent;
