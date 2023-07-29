f1c9f6660dab8e773396aadad0b497e8b49cf830
fderen-dev <fderen.dev@gmail.com>
2023-07-29 14:22:40 +0200

    Commented out not used tags

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,17 +5,17 @@ export const ROUTES = {
 };
 
 export const TAGS: Record<string, Tag> = {
-  angular: {
-    id: "1",
-    name: "angular",
-    path: "angular",
-    url: "blog?tag=angular",
-  },
-  "react.js": {
-    id: "2",
-    name: "react.js",
-    path: "react.js",
-    url: "blog?tag=react.js",
-  },
-  sass: { id: "3", name: "sass", path: "sass", url: "blog?tag=sass" },
+  // angular: {
+  //   id: "1",
+  //   name: "angular",
+  //   path: "angular",
+  //   url: "blog?tag=angular",
+  // },
+  // "react.js": {
+  //   id: "2",
+  //   name: "react.js",
+  //   path: "react.js",
+  //   url: "blog?tag=react.js",
+  // },
+  // sass: { id: "3", name: "sass", path: "sass", url: "blog?tag=sass" }
 };
