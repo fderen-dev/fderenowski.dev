2c8b6cc356ad63dca2dce1d4c29711a989689e58
fderen-dev <fderen.dev@gmail.com>
2023-08-24 00:14:44 +0200

    Added react tag

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,17 +5,10 @@ export const ROUTES = {
 };
 
 export const TAGS: Record<string, Tag> = {
-  // angular: {
-  //   id: "1",
-  //   name: "angular",
-  //   path: "angular",
-  //   url: "blog?tag=angular",
-  // },
-  // "react.js": {
-  //   id: "2",
-  //   name: "react.js",
-  //   path: "react.js",
-  //   url: "blog?tag=react.js",
-  // },
-  // sass: { id: "3", name: "sass", path: "sass", url: "blog?tag=sass" }
+  "react": {
+    id: "1",
+    name: "react",
+    path: "react",
+    url: "blog?tag=react",
+  },
 };
