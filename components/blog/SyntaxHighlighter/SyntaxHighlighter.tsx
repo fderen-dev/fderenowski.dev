a7d2962ddefe3ff8252a29ec947cb8e3055ed5d8
fderen-dev <fderen.dev@gmail.com>
2023-08-24 00:12:33 +0200

    Extended SyntaxHighlighter supported languages

--- components/blog/SyntaxHighlighter/SyntaxHighlighter.tsx
+++ components/blog/SyntaxHighlighter/SyntaxHighlighter.tsx
@@ -1,10 +1,22 @@
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import tsx from "react-syntax-highlighter/dist/esm/languages/prism/tsx";
-import materialDark from "react-syntax-highlighter/dist/esm/styles/prism/material-dark";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+import javascript from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import materialDark from "react-syntax-highlighter/dist/cjs/styles/prism/material-dark";
 
 SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("javascript", javascript);
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("jsx", jsx);
 
-type SyntaxHiglighterLanguage = "tsx" | "javascript" | "typescript" | "css" | "html" | "scss";
+type SyntaxHiglighterLanguage = "jsx" | "javascript" | "tsx" | "typescript" | "css" | "scss" | "bash";
 
 interface SyntaxHiglighterProps {
   codeString: string;
