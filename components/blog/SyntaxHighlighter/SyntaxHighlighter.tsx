4b6efd06d6a5754ac2516ec334cfd7a27d90bcf4
fderen-dev <fderen.dev@gmail.com>
2023-08-24 21:21:31 +0200

    Fixed higlighter imports, changed style to vsc dark, added json language

--- components/blog/SyntaxHighlighter/SyntaxHighlighter.tsx
+++ components/blog/SyntaxHighlighter/SyntaxHighlighter.tsx
@@ -1,12 +1,13 @@
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
 import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
 import javascript from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
 import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
 import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
 import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
 import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
-import materialDark from "react-syntax-highlighter/dist/cjs/styles/prism/material-dark";
+import vscDarkPlus from "react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus"; 
 
 SyntaxHighlighter.registerLanguage("tsx", tsx);
 SyntaxHighlighter.registerLanguage("bash", bash);
@@ -15,16 +16,30 @@ SyntaxHighlighter.registerLanguage("javascript", javascript);
 SyntaxHighlighter.registerLanguage("css", css);
 SyntaxHighlighter.registerLanguage("scss", scss);
 SyntaxHighlighter.registerLanguage("jsx", jsx);
+SyntaxHighlighter.registerLanguage("json", json);
 
-type SyntaxHiglighterLanguage = "jsx" | "javascript" | "tsx" | "typescript" | "css" | "scss" | "bash";
+type SyntaxHiglighterLanguage = "jsx" | "javascript" | "tsx" | "typescript" | "css" | "scss" | "bash" | "json";
 
 interface SyntaxHiglighterProps {
   codeString: string;
-  language: SyntaxHiglighterLanguage
+  language: SyntaxHiglighterLanguage,
+  showLineNumbers: boolean,
+  wrapLines: boolean,
+
 }
 
-export const SyntaxHiglighter = ({codeString, language}: SyntaxHiglighterProps) => (
-  <SyntaxHighlighter language={language} style={materialDark}>
+export const SyntaxHiglighter = ({
+  codeString,
+  language,
+  showLineNumbers = false,
+  wrapLines = false,
+}: SyntaxHiglighterProps) => (
+  <SyntaxHighlighter
+    language={language}
+    style={vscDarkPlus}
+    showLineNumbers={showLineNumbers}
+    wrapLines={wrapLines}
+  >
     {codeString}
   </SyntaxHighlighter>
 );
