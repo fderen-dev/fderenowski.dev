5c3e6eea2619f2c9bb62917a0f6990dd8002ed48
fderen-dev <fderen.dev@gmail.com>
2023-10-24 21:29:43 +0200

    Updated placeholders for seo slice

--- slices/Seo/index.tsx
+++ slices/Seo/index.tsx
@@ -1,5 +1,8 @@
 import { Content } from "@prismicio/client";
+import { asLink,asText } from "@prismicio/helpers";
 import { SliceComponentProps } from "@prismicio/react";
+
+import { Head } from "components/common/Head/Head";;
 
 /**
  * Props for `Seo`.
@@ -10,14 +13,17 @@ export type SeoProps = SliceComponentProps<Content.SeoSlice>;
  * Component for "Seo" Slices.
  */
 const Seo = ({ slice }: SeoProps): JSX.Element => {
+  const { meta_og_image, meta_og_description, meta_og_url, ...meta } = slice.primary;
+
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for seo (variation: {slice.variation}) Slices
-    </section>
+    <Head
+      meta_og_image={meta_og_image.url!}
+      meta_og_description={asText(meta_og_description)}
+      // TODO: link resolver
+      meta_og_url={null}
+      {...meta}
+    />
   );
-};
+}
 
 export default Seo;
