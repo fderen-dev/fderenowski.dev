81d0a7bb6c139180f29072d3d2b87b0b25200227
fderen-dev <fderen.dev@gmail.com>
2023-11-27 22:51:18 +0100

    Added snippets tag

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,10 +5,16 @@ export const ROUTES = {
 };
 
 export const TAGS: Record<string, Tag> = {
-  "react": {
+  react: {
     id: "1",
     name: "react",
     path: "react",
     url: "blog?tag=react",
+  },
+  snippets: {
+    id: "2",
+    name: "snippets",
+    path: "snippets",
+    url: "blog?tag=snippets",
   },
 };
